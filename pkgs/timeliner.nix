b114348cbb5e47f24760b091469f4c0384b0b4b7
Nicolas Bareil <nico@chdir.org>
2024-08-05 11:59:34 +0200

    Update timeliner

--- pkgs/timeliner.nix
+++ pkgs/timeliner.nix
@@ -7,11 +7,11 @@ buildGoModule rec {
     src = fetchFromGitHub {
       owner = "airbus-cert";
       repo = name;
-      rev = "b068f5f164eae9a729989697ff25656f86a67287";
-      hash = "sha256-qWqwM/AIFMZ6giAEqEa9GhJFbUzARiudWVaN2fGrlJA=";
+      rev = "a41292eec4bb99e5c253343b4e426dee1858906c";
+      hash = "sha256-KwYguwCNSE1elKbyiWFf6nuRs67GVAxEMAPsQJSU1PE=";
     };
 
-    vendorHash = "sha256-L0EQy6AoaoJrcSDSTx4owhGmUtlMH60Okr5fL7rDhzQ=";
+    vendorHash = "sha256-m/VyKp0mTLAhjLJIhKBYGauavzk09SYlO94SwuQ2icw=";
 
     postInstall = ''
       mv $out/bin/main $out/bin/timeliner
